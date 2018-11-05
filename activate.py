a10c23aee52c4c163de57f0858d1e889db5a800e
Chris Laws <christopherlaws85@gmail.com>
2018-11-05 14:27:36 +0000

    First full commit

---
+++ activate.py
@@ -0,0 +1,4 @@
+import requests
+
+ifttt_webhook_url = 'https://maker.ifttt.com/trigger/test_event/with/key/pcHVrlb_w7ktzvswMmVmu4b11FSmYyyhIqGwDnSfMA-'
+requests.post(ifttt_webhook_url)
---
+++ setup.py
@@ -0,0 +1,9 @@
+import requests
+bitcoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
+response = requests.get(bitcoin_api_url)
+response_json = response.json()
+type(response_json)
+
+print(response_json[0])
+
+
